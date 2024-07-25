b7686b16951d2c671f86e7f15774018fa463131a
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-07-25 10:21:05 +0530

    Create Programs.cs

---
+++ Hackerrank Probs/C#/Programs.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hackerrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

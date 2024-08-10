964b9dd6f7e407e79dd90af9af789e7b880ae848
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-10 11:55:05 +0530

    Create bst.py

---
+++ Data Structures/Binary Tree/bst.py
@@ -0,0 +1,20 @@
+def remove_none(nums):
+    return [x for x in nums if x is not None]
+
+def is_bst(node):
+    if node is None:
+        return True, None, None
+    
+    is_bst_l, min_l, max_l = is_bst(node.left)
+    is_bst_r, min_r, max_r = is_bst(node.right)
+    
+    is_bst_node = (is_bst_l and is_bst_r and 
+              (max_l is None or node.key > max_l) and 
+              (min_r is None or node.key < min_r))
+    
+    min_key = min(remove_none([min_l, node.key, min_r]))
+    max_key = max(remove_none([max_l, node.key, max_r]))
+    
+    # print(node.key, min_key, max_key, is_bst_node)
+        
+    return is_bst_node, min_key, max_key

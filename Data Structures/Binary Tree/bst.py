82d4a4d08ed8ec59ea452228592f5a03ccc00653
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-10 11:57:27 +0530

    Update bst.py

--- Data Structures/Binary Tree/bst.py
+++ Data Structures/Binary Tree/bst.py
@@ -18,3 +18,54 @@ def is_bst(node):
     # print(node.key, min_key, max_key, is_bst_node)
         
     return is_bst_node, min_key, max_key
+
+
+#storing 
+class BSTNode():
+    def __init__(self, key, value=None):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+        
+        
+#insertion 
+def insert(node, key, value):
+    if node is None:
+        node = BSTNode(key, value)
+    elif key < node.key:
+        node.left = insert(node.left, key, value)
+        node.left.parent = node
+    elif key > node.key:
+        node.right = insert(node.right, key, value)
+        node.right.parent = node
+    return node
+
+
+#fidnding a node
+def find(node, key):
+    if node is None:
+        return None
+    if key == node.key:
+        return node
+    if key < node.key:
+        return find(node.left, key)
+    if key > node.key:
+        return find(node.right, key)
+
+
+#updating bst 
+def update(node, key, value):
+    target = find(node, key)
+    if target is not None:
+        target.value = value
+
+
+#list all nodes
+def list_all(node):
+    if node is None:
+        return []
+    return list_all(node.left) + [(node.key, node.value)] + list_all(node.right)
+
+

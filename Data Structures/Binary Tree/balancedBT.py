b96a6e51f81add785dd0500b3b9900e6e2367806
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-10 11:57:59 +0530

    Create balancedBT.py

---
+++ Data Structures/Binary Tree/balancedBT.py
@@ -0,0 +1,8 @@
+def is_balanced(node):
+    if node is None:
+        return True, 0
+    balanced_l, height_l = is_balanced(node.left)
+    balanced_r, height_r = is_balanced(node.right)
+    balanced = balanced_l and balanced_r and abs(height_l - height_r) <=1
+    height = 1 + max(height_l, height_r)
+    return balanced, height

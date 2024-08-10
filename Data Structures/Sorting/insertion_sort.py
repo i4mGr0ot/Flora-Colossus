b58a4a8a5480ae269584cde7cb637024f0e37299
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-10 13:42:05 +0530

    Create insertion_sort.py

---
+++ Data Structures/Sorting/insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(nums):
+    nums = list(nums)
+    for i in range(len(nums)):
+        cur = nums.pop(i)
+        j = i-1
+        while j >=0 and nums[j] > cur:
+            j -= 1
+        nums.insert(j+1, cur)
+    return nums            

5b6ffbd9cbcbfbab01392508075540c58b807f04
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-10 11:48:07 +0530

    Create btree.py

---
+++ Data Structures/Binary Tree/btree.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+      
+def parse_tuple(data):
+    # print(data)
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+    return node
+tree2 = parse_tuple(((1,3,None), 2, ((None, 3, 4), 5, (6, 7, 8))))
+
+def display_keys(node, space='\t', level=0):
+    # print(node.key if node else None, level)
+    
+    # If the node is empty
+    if node is None:
+        print(space*level + '∅')
+        return   
+    
+    # If the node is a leaf 
+    if node.left is None and node.right is None:
+        print(space*level + str(node.key))
+        return
+    
+    # If the node has children
+    display_keys(node.right, space, level+1)
+    print(space*level + str(node.key))
+    display_keys(node.left,space, level+1)    

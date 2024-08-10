3c2a6d8c5ad3fc2999c4fb9c179fa76cde2012db
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-08-10 13:40:24 +0530

    Create test.py

---
+++ Data Structures/Sorting/Bubble Sort/test.py
@@ -0,0 +1,7 @@
+nums0, output0 = test0['input']['nums'], test0['output']
+
+print('Input:', nums0)
+print('Expected output:', output0)
+result0 = bubble_sort(nums0)
+print('Actual output:', result0)
+print('Match:', result0 == output0)
